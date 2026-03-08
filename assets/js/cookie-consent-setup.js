ca7177b58ba4f64d13d071a82e96e3d087670b31
kathryncaruso <91213455+kathryncaruso@users.noreply.github.com>
2026-03-08 20:55:07 +0000

    Deploying to gh-pages from @ kathryncaruso/kathryncaruso.github.io@883b7f917b6ba984d4145fd9500ae99389cfa6db 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://katie-caruso.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://kathryncaruso.github.io/#contact">contact us</a>.'
               }
             ]
           }
--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "katie-caruso/katie-caruso.github.io",
+    "data-repo": "kathryncaruso/kathryncaruso.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
